e47766a441d70e319394eb5758be1f68858948a5
Prelang Builder <builder@prelang.com>
2014-07-18 22:48:56 -0500

    Executed 'bundle install' to fetch and install required Ruby Gems

---
+++ db/seeds/charities.seeds.rb
@@ -0,0 +1,17 @@
+charities = [
+  'American Heart Association',
+  'Living Waters',
+  'MAke-A-Wish',
+  'Operation Blessing',
+  "Samaritan's Purse",
+  'St. Jude'
+]
+
+charities.each do |name|
+  unless Charity.exists?(name: name)
+    Charity.create(name: name)
+    puts name
+  end
+end
+
+puts 'Charities seeded.'
